c8466e2ffde7d76d03fac31d819f4a3b0aa83519
allenash0126 <allen64031@hotmail.com>
2023-12-05 20:53:59 +0800

    build: project init_server

--- shortener.js
+++ shortener.js
@@ -1,8 +1,20 @@
-const urlInput = document.querySelector('#url-input')
-const urlForm = document.querySelector('#url-form')
+// const urlInput = document.querySelector('#url-input')
+// const urlForm = document.querySelector('#url-form')
+const express = require('express')
+const app = express()
+const port = 3001
 
-urlForm.addEventListener('submit', function onURLSubmitted(event) {
-  const urlOriginal = urlInput.value
-  event.preventDefault()
-  console.log(urlOriginal)
+app.get('/',(req,res) => {
+  res.send('express app for shorten-URL')
 })
+
+app.listen(port,() => {
+  console.log(`express server is running on http://localhost:${port}`)
+})
+
+
+// urlForm.addEventListener('submit', function onURLSubmitted(event) {
+//   const urlOriginal = urlInput.value
+//   event.preventDefault()
+//   console.log(urlOriginal)
+// })
