ece208069c1e479a0ba965469b65d3a1cb519b4b
allenash0126 <allen64031@hotmail.com>
2023-12-11 17:19:36 +0800

    Build : Copy button

--- shortener.js
+++ shortener.js
@@ -3,13 +3,15 @@ const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 let array = []
-let baseURL = 'http://localhost:3000/id?url='
+const baseURL = 'http://localhost:3000/id?url='
+const ncp = require("copy-paste"); 
 
 app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 app.use(express.static('public'))
 
+// 產出用於短址的5碼亂數
 function getRandomURL(quantityOfUnit) {
   let component = [0,1,2,3,4,5,6,7,8,9,'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
   let URL = ''
@@ -24,6 +26,7 @@ app.get('/',(req,res) => {
     res.render('index')
 })
 
+// 產出短址
 app.get('/URLs', (req, res) => {
   // res.render('index')
   let keywords = req.query.url
@@ -48,6 +51,7 @@ app.get('/URLs', (req, res) => {
     }
 })
 
+// 當user貼上短址後，重新導向原本的網址
 app.get('/id',(req,res) => {  
   let newKeywords = req.query.url
   console.log(newKeywords)
@@ -55,6 +59,12 @@ app.get('/id',(req,res) => {
   console.log(URL_FindOriginal)
   console.log(array[URL_FindOriginal])
   res.redirect(array[URL_FindOriginal])
+})
+
+// Copy Button
+app.get('/buttonClicked',(req,res) => {
+  let copyURL = array[array.length-1]
+  ncp.copy(copyURL, function () {})
 })
 
 app.listen(port,() => {
